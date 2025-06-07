608681a5acc03d9485aac5f676421f487b794aac
John Milius <mil24086@byui.edu>
2025-06-07 15:51:26 -0600

    new schema

--- gym_tracker_workbook.sql
+++ gym_tracker_workbook.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema gym_tracker
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `gym_tracker` ;
@@ -12,40 +15,21 @@ DROP SCHEMA IF EXISTS `gym_tracker` ;
 -- -----------------------------------------------------
 -- Schema gym_tracker
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `gym_tracker` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `gym_tracker` DEFAULT CHARACTER SET utf8mb3 ;
 USE `gym_tracker` ;
 
 -- -----------------------------------------------------
--- Table `gym_tracker`.`personal_weight`
+-- Table `gym_tracker`.`exercise`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `gym_tracker`.`personal_weight` ;
+DROP TABLE IF EXISTS `gym_tracker`.`exercise` ;
 
-CREATE TABLE IF NOT EXISTS `gym_tracker`.`personal_weight` (
-  `personal_weight_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `weight` FLOAT NOT NULL,
-  `date` DATE NOT NULL,
-  PRIMARY KEY (`personal_weight_id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `gym_tracker`.`user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `gym_tracker`.`user` ;
-
-CREATE TABLE IF NOT EXISTS `gym_tracker`.`user` (
-  `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `personal_weight_id` INT UNSIGNED NOT NULL,
-  `user_name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  INDEX `fk_user_personal_weight1_idx` (`personal_weight_id` ASC) VISIBLE,
-  CONSTRAINT `fk_user_personal_weight1`
-    FOREIGN KEY (`personal_weight_id`)
-    REFERENCES `gym_tracker`.`personal_weight` (`personal_weight_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `gym_tracker`.`exercise` (
+  `exercise_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `exercise_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`exercise_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 26
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -57,19 +41,9 @@ CREATE TABLE IF NOT EXISTS `gym_tracker`.`exercise_category` (
   `exercise_category_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`exercise_category_id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `gym_tracker`.`exercise`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `gym_tracker`.`exercise` ;
-
-CREATE TABLE IF NOT EXISTS `gym_tracker`.`exercise` (
-  `exercise_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `exercise_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`exercise_id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -81,7 +55,9 @@ CREATE TABLE IF NOT EXISTS `gym_tracker`.`movement_type` (
   `movement_type_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `movement` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`movement_type_id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 16
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -98,49 +74,53 @@ CREATE TABLE IF NOT EXISTS `gym_tracker`.`exercise_entry` (
   INDEX `fk_session_exercise_exercise_category1_idx` (`exercise_category_id` ASC) VISIBLE,
   INDEX `fk_session_exercise_exercise1_idx` (`exercise_id` ASC) VISIBLE,
   INDEX `fk_session_exercise_movement_type1_idx` (`movement_type_id` ASC) VISIBLE,
-  CONSTRAINT `fk_session_exercise_exercise_category1`
-    FOREIGN KEY (`exercise_category_id`)
-    REFERENCES `gym_tracker`.`exercise_category` (`exercise_category_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_session_exercise_exercise1`
     FOREIGN KEY (`exercise_id`)
-    REFERENCES `gym_tracker`.`exercise` (`exercise_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `gym_tracker`.`exercise` (`exercise_id`),
+  CONSTRAINT `fk_session_exercise_exercise_category1`
+    FOREIGN KEY (`exercise_category_id`)
+    REFERENCES `gym_tracker`.`exercise_category` (`exercise_category_id`),
   CONSTRAINT `fk_session_exercise_movement_type1`
     FOREIGN KEY (`movement_type_id`)
-    REFERENCES `gym_tracker`.`movement_type` (`movement_type_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `gym_tracker`.`movement_type` (`movement_type_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `gym_tracker`.`workout_session`
+-- Table `gym_tracker`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `gym_tracker`.`workout_session` ;
+DROP TABLE IF EXISTS `gym_tracker`.`user` ;
 
-CREATE TABLE IF NOT EXISTS `gym_tracker`.`workout_session` (
-  `workout_session_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `gym_tracker`.`user` (
+  `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`user_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `gym_tracker`.`personal_weight`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `gym_tracker`.`personal_weight` ;
+
+CREATE TABLE IF NOT EXISTS `gym_tracker`.`personal_weight` (
+  `personal_weight_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` INT UNSIGNED NOT NULL,
-  `exercise_entry_id` INT UNSIGNED NOT NULL,
+  `weight` FLOAT NOT NULL,
   `date` DATE NOT NULL,
-  `notes` VARCHAR(100) NULL,
-  PRIMARY KEY (`workout_session_id`),
-  INDEX `fk_workout_session_user1_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_workout_session_exercise_entry1_idx` (`exercise_entry_id` ASC) VISIBLE,
-  CONSTRAINT `fk_workout_session_user1`
+  PRIMARY KEY (`personal_weight_id`),
+  INDEX `fk_personal_weight_user1_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_personal_weight_user1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `gym_tracker`.`user` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_workout_session_exercise_entry1`
-    FOREIGN KEY (`exercise_entry_id`)
-    REFERENCES `gym_tracker`.`exercise_entry` (`exercise_entry_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `gym_tracker`.`user` (`user_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 27
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -157,10 +137,10 @@ CREATE TABLE IF NOT EXISTS `gym_tracker`.`set_log` (
   INDEX `fk_set_log_exercise_entry1_idx` (`exercise_entry_id` ASC) VISIBLE,
   CONSTRAINT `fk_set_log_exercise_entry1`
     FOREIGN KEY (`exercise_entry_id`)
-    REFERENCES `gym_tracker`.`exercise_entry` (`exercise_entry_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `gym_tracker`.`exercise_entry` (`exercise_entry_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 161
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -173,7 +153,9 @@ CREATE TABLE IF NOT EXISTS `gym_tracker`.`subscriptions` (
   `price` FLOAT NOT NULL,
   `subscription_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`subscriptions_id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -192,15 +174,38 @@ CREATE TABLE IF NOT EXISTS `gym_tracker`.`subscriptions_has_user` (
   INDEX `fk_subscriptions_has_user_subscriptions1_idx` (`subscriptions_id` ASC) VISIBLE,
   CONSTRAINT `fk_subscriptions_has_user_subscriptions1`
     FOREIGN KEY (`subscriptions_id`)
-    REFERENCES `gym_tracker`.`subscriptions` (`subscriptions_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `gym_tracker`.`subscriptions` (`subscriptions_id`),
   CONSTRAINT `fk_subscriptions_has_user_user1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `gym_tracker`.`user` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `gym_tracker`.`user` (`user_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `gym_tracker`.`workout_session`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `gym_tracker`.`workout_session` ;
+
+CREATE TABLE IF NOT EXISTS `gym_tracker`.`workout_session` (
+  `workout_session_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` INT UNSIGNED NOT NULL,
+  `exercise_entry_id` INT UNSIGNED NOT NULL,
+  `date` DATE NOT NULL,
+  `notes` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`workout_session_id`),
+  INDEX `fk_workout_session_user1_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_workout_session_exercise_entry1_idx` (`exercise_entry_id` ASC) VISIBLE,
+  CONSTRAINT `fk_workout_session_exercise_entry1`
+    FOREIGN KEY (`exercise_entry_id`)
+    REFERENCES `gym_tracker`.`exercise_entry` (`exercise_entry_id`),
+  CONSTRAINT `fk_workout_session_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `gym_tracker`.`user` (`user_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 51
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
